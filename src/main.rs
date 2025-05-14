16208d138053df2fb1b3d9aece57e63545e1f3c0
Cameron <cameronh63@gmail.com>
2025-05-15 00:00:00 +0200

    feat: Beginning of Storage system

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,92 @@
+use std::io::{BufReader, Write, stdin, stdout};
+
+use broker::Broker;
+use config::Configuration;
+
 pub mod broker;
 pub mod config;
+pub mod storage;
+pub mod index;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    tracing_subscriber::fmt()
+        .pretty()
+        .with_thread_names(true)
+        // enable everything
+        .with_max_level(tracing::Level::TRACE)
+        // sets this to be the default, global collector for this application.
+        .init();
+
+
+    let config = Configuration::from_env();
+
+    let broker = Broker::from_config(config);
+
+    tracing::info!("Created Broker: {:#?}", broker);
+
+    loop {
+        print!("> ");
+        stdout().flush().unwrap();
+        if let Some(Ok(input)) = stdin().lines().next() {
+            if input.trim() == "exit" {
+                break;
+            }
+            if input.trim().is_empty() {
+                continue;
+            }
+
+            let tokens = input.split_whitespace();
+
+            let tokens = tokens.take(3).map(|s| s.to_string()).collect::<Vec<_>>();
+
+            let command = tokens.first().expect("NO Command Given.");
+
+            match command.as_ref() {
+                "produce" => {
+                    let name = tokens.get(1).expect("Invalid Message.");
+
+                    let data_file = tokens.get(2).expect("Invalid Data Path.");
+
+                    let data = std::fs::File::open(data_file).expect("No data found.");
+
+                    println!("Finding stream for name: {name}");
+
+                    let (schema, _tx, _rx) = broker
+                        .get_stream(name)
+                        .expect("Could not find stream for stream_name.");
+
+                    let mut json = arrow_json::ReaderBuilder::new(schema.clone())
+                        .build(BufReader::new(data))
+                        .unwrap();
+
+                    let batch = json.next().unwrap().unwrap();
+
+                    println!("You are producing! {:#?}", batch);
+
+                    broker.produce(name, batch).await;
+                }
+                "listen" => {
+                    let name = tokens.get(1).expect("Invalid Message.").clone();
+
+                    let mut rx = broker
+                        .get_receiver(&name)
+                        .expect("Could not find stream for stream_name.");
+
+                    tokio::spawn(async move {
+                        while let Ok(value) = rx.recv().await {
+                            println!("Received value {:#?} on stream {name}", value);
+                        }
+                    });
+                }
+                _ => {
+                    println!("Invalid Command: {:#?}", tokens);
+                    continue;
+                }
+            }
+        }
+    }
+
     Ok(())
 }
