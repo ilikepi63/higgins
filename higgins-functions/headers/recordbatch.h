624fe43afaa007a65d3fd57d052d26d931c17cd9
Cameron <cameronh63@gmail.com>
2025-05-30 00:00:00 +0200

    feat: Working towards RecordBatch from ArrayData

--- higgins-functions/headers/recordbatch.h
+++ higgins-functions/headers/recordbatch.h
@@ -1,9 +1,9 @@
 #include <stdint.h>
 #include "arrow.h"
 
-struct WasmRecordBatch
+struct FFIRecordBatch
 {
-    struct ArrowSchema *schema;
     int64_t n_columns;
+    struct ArrowSchema **schema;
     struct ArrowArray **columns;
 };
--- higgins-functions/src/types/record_batch.rs
+++ higgins-functions/src/types/record_batch.rs
@@ -3,7 +3,7 @@ use super::{WasmArrowArray, WasmArrowSchema, WasmPtr};
 #[repr(C)]
 #[derive(Debug)]
 pub struct WasmRecordBatch {
-    pub schema: WasmPtr<WasmArrowSchema>,
     pub n_columns: i64,
+    pub schema: WasmPtr<WasmPtr<WasmArrowSchema>>,
     pub columns: WasmPtr<WasmPtr<WasmArrowArray>>,
 }
