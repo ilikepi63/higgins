4baa32cf4514cfad08aca7cc6ca6f83fdc933736
Cameron <cameronh63@gmail.com>
2025-06-09 12:15:30 +0200

    feat: Initial setup for client server communication

---
+++ higgins-codec/src/lib.rs
@@ -0,0 +1,51 @@
+mod codec {
+    include!(concat!(env!("OUT_DIR"), "/higgins.rs"));
+}
+
+pub use codec::*; // TODO: everything visible in codec here? 
+
+#[cfg(test)]
+mod test {
+
+    use std::io::Cursor;
+
+    use prost::Message as _;
+
+    use crate::message::Type;
+
+    use super::*;
+
+    #[test]
+    fn can_serde_correctly() {
+        // Send a ping command to the server;
+
+        let mut buf = Vec::new();
+
+        let ping = Ping::default();
+
+        let message = Message {
+            r#type: Type::Ping as i32,
+            consume_request: None,
+            consume_response: None,
+            produce_request: None,
+            produce_response: None,
+            metadata_request: None,
+            metadata_response: None,
+            ping: Some(ping),
+            pong: None,
+        };
+
+        buf.reserve(message.encoded_len());
+
+        message.encode(&mut buf).unwrap();
+
+        println!("{:#?}", buf);
+
+
+        let decode = Message::decode(buf.as_ref()).unwrap();
+
+        println!("Decoded: {:#?}", decode);
+
+        panic!();
+    }
+}
