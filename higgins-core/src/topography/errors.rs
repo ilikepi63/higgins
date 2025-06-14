05db84d55536139e7642b7319cbdf8b6b98d56da
Cameron <cameronh63@gmail.com>
2025-06-14 11:38:52 +0200

    feat: Added Checks for importing schema + streams

--- higgins-core/src/topography/errors.rs
+++ higgins-core/src/topography/errors.rs
@@ -4,4 +4,8 @@ use thiserror::Error;
 pub enum TopographyError {
     #[error("The entry at the designated key is already occupied: {0}")]
     Occupied(String),
+    #[error("Derivative stream not found in topography: {0}")]
+    DerivativeNotFound(String),
+    #[error("Schema not found in topography: {0}")]
+    SchemaNotFound(String),
 }
