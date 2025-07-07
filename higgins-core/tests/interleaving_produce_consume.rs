4da87bfaf4a61e65da4d4fb8742becccfe0e2122
Cameron <cameronh63@gmail.com>
2025-07-07 15:13:09 +0200

    feat: Implemented the interleaving loom test

--- higgins-core/tests/interleaving_produce_consume.rs
+++ higgins-core/tests/interleaving_produce_consume.rs
@@ -8,11 +8,14 @@ use higgins_codec::{
     TakeRecordsRequest, message::Type,
 };
 use prost::Message as _;
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
-};
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
 use tracing_test::traced_test;
+
+use crate::common::{consume, create_subscription, produce};
+
+mod common;
 
 #[tokio::test]
 #[traced_test]
@@ -37,9 +40,7 @@ async fn can_correctly_consume_and_produce_interleaving_requests() {
 
     tokio::time::sleep(Duration::from_millis(100)).await;
 
-    let mut socket = TcpStream::connect(format!("127.0.0.1:{port}"))
-        .await
-        .unwrap();
+    let mut socket = TcpStream::connect(format!("127.0.0.1:{port}")).unwrap();
 
     // 1. Do a basic Ping test.
     let ping = Ping::default();
@@ -54,12 +55,9 @@ async fn can_correctly_consume_and_produce_interleaving_requests() {
 
     tracing::info!("Writing: {:#?}", write_buf);
 
-    let _result = socket.write_all(&write_buf).await.unwrap();
+    let _result = socket.write_all(&write_buf).unwrap();
 
-    let n = tokio::time::timeout(Duration::from_secs(5), socket.read(&mut read_buf))
-        .await
-        .unwrap()
-        .unwrap();
+    let n = socket.read(&mut read_buf).unwrap();
 
     assert_ne!(n, 0);
 
@@ -87,12 +85,9 @@ async fn can_correctly_consume_and_produce_interleaving_requests() {
     .encode(&mut write_buf)
     .unwrap();
 
-    let _result = socket.write_all(&write_buf).await.unwrap();
+    let _result = socket.write_all(&write_buf).unwrap();
 
-    let n = tokio::time::timeout(Duration::from_secs(5), socket.read(&mut read_buf))
-        .await
-        .unwrap()
-        .unwrap();
+    let n = socket.read(&mut read_buf).unwrap();
 
     assert_ne!(n, 0);
 
@@ -108,142 +103,69 @@ async fn can_correctly_consume_and_produce_interleaving_requests() {
     }
 
     // Start a subscription on that stream.
-    let create_subscription = CreateSubscriptionRequest {
-        offset: None,
-        offset_type: 0,
-        timestamp: None,
-        stream_name: "update_customer".as_bytes().to_vec(),
-    };
-
-    let mut write_buf = BytesMut::new();
-    let mut read_buf = BytesMut::zeroed(1024);
-
-    Message {
-        r#type: Type::Createsubscriptionrequest as i32,
-        create_subscription_request: Some(create_subscription),
-        ..Default::default()
-    }
-    .encode(&mut write_buf)
-    .unwrap();
-
-    let _result = socket.write_all(&write_buf).await.unwrap();
-
-    let n = tokio::time::timeout(Duration::from_secs(5), socket.read(&mut read_buf))
-        .await
-        .unwrap()
-        .unwrap();
-
-    assert_ne!(n, 0);
-
-    let slice = &read_buf[0..n];
-
-    let message = Message::decode(slice).unwrap();
-
-    let sub_id = match Type::try_from(message.r#type).unwrap() {
-        Type::Createsubscriptionresponse => {
-            let sub_id = message
-                .create_subscription_response
-                .unwrap()
-                .subscription_id;
-
-            tracing::info!("Got the sub_id: {:#?}", sub_id);
-
-            sub_id.unwrap()
-        }
-        _ => panic!("Received incorrect response from server for Create Subscription request."),
-    };
+    let sub_id = create_subscription("update_customer".as_bytes(), &mut socket).unwrap();
 
     // Produce to the stream.
-
     let payload = std::fs::read_to_string("tests/customer.json").unwrap();
 
-    let produce_request = ProduceRequest {
-        partition_key: "test_partition".as_bytes().to_vec(),
-        payload: payload.as_bytes().to_vec(),
-        stream_name: "update_customer".as_bytes().to_vec(),
-    };
+    loom::model(move || {
+        let mut socket = std::net::TcpStream::connect(format!("127.0.0.1:{port}")).unwrap();
 
-    let mut write_buf = BytesMut::new();
-    let mut read_buf = BytesMut::zeroed(1024);
+        let socket = loom::sync::Arc::new(loom::sync::Mutex::new(socket));
+        let result_collection = loom::sync::Arc::new(loom::sync::Mutex::new(vec![]));
 
-    Message {
-        r#type: Type::Producerequest as i32,
-        produce_request: Some(produce_request),
-        ..Default::default()
-    }
-    .encode(&mut write_buf)
-    .unwrap();
+        // this likely would be better in a constant.
+        let message_count = 100;
 
-    let _result = socket.write_all(&write_buf).await.unwrap();
+        let produce_handles = (0..message_count).map(|_| {
+            let payload = payload.clone();
+            let socket = socket.clone(); // arc clone. 
 
-    let n = tokio::time::timeout(Duration::from_secs(5), socket.read(&mut read_buf))
-        .await
-        .unwrap()
-        .unwrap();
+            let handle = loom::thread::spawn(move || {
+                loom::future::block_on(async {
+                    let mut socket = socket.lock().unwrap();
 
-    assert_ne!(n, 0);
+                    produce(
+                        "update_customer".as_bytes(),
+                        "test_partition".as_bytes(),
+                        payload.as_bytes(),
+                        &mut socket,
+                    )
+                });
+            });
 
-    let slice = &read_buf[0..n];
+            handle
+        });
 
-    let message = Message::decode(slice).unwrap();
+        let consume_handles = (0..message_count).map(|_| {
+            let socket = socket.clone(); // arc clone. 
+            let sub_id = sub_id.clone();
+            let result_collection = result_collection.clone();
 
-    match Type::try_from(message.r#type).unwrap() {
-        Type::Produceresponse => {
-            let message = message.produce_response;
+            let handle = loom::thread::spawn(move || {
+                loom::future::block_on(async {
+                    let mut socket = socket.lock().unwrap();
 
-            tracing::info!("Received produce response: {:#?}", message);
+                    // Consume from the stream.
+                    let response = consume(sub_id, &mut socket).unwrap();
+
+                    let mut collection_lock = result_collection.lock().unwrap();
+
+                    collection_lock.push(response);
+                });
+            });
+
+            handle
+        });
+
+        // Join the given handles.
+        for handle in produce_handles.chain(consume_handles) {
+            handle.join().unwrap();
         }
-        _ => panic!("Received incorrect response from server for Create Subscription request."),
-    }
 
-    // Consume from the stream.
-    let take_request = TakeRecordsRequest {
-        n: 1,
-        subscription_id: sub_id,
-        stream_name: "update_customer".as_bytes().to_vec(),
-    };
+        let received_values = result_collection.lock().unwrap();
+        assert_eq!(message_count, received_values.len());
 
-    let mut write_buf = BytesMut::new();
-    let mut read_buf = BytesMut::zeroed(8048);
-
-    Message {
-        r#type: Type::Takerecordsrequest as i32,
-        take_records_request: Some(take_request),
-        ..Default::default()
-    }
-    .encode(&mut write_buf)
-    .unwrap();
-
-    let _result = socket.write_all(&write_buf).await.unwrap();
-
-    let n = tokio::time::timeout(Duration::from_secs(5), socket.read(&mut read_buf))
-        .await
-        .unwrap()
-        .unwrap();
-
-    assert_ne!(n, 0);
-
-    let slice = &read_buf[0..n];
-
-    let message = Message::decode(slice).unwrap();
-
-    match Type::try_from(message.r#type).unwrap() {
-        Type::Takerecordsresponse => {
-            tracing::info!("Receieved a take records response!");
-
-            let take_records_response = message.take_records_response.unwrap();
-
-            tracing::info!("Records_Response: {:#?}", take_records_response);
-
-            for record in take_records_response.records.iter() {
-
-                tracing::info!("{}", String::from_utf8(record.data.clone()).unwrap());
-
-            }
-
-        }
-        _ => panic!("Received incorrect response from server for Create Subscription request."),
-    }
-
-    handle.abort();
+        handle.abort();
+    });
 }
