ba3adf71a7e6cea8c50c41bb496ca931ac3c7961
Cameron <cameronh63@gmail.com>
2025-07-13 20:11:44 +0200

    feat: Initial Query by Timestamp implementation

---
+++ higgins-core/src/utils/consumption.rs
@@ -0,0 +1,47 @@
+use higgins_codec::{GetIndexResponse, TakeRecordsResponse};
+use riskless::messages::ConsumeResponse;
+use tokio::sync::mpsc::Receiver;
+use higgins_codec::Record;
+
+use crate::storage::arrow_ipc::read_arrow;
+pub async fn collect_consume_responses(
+    mut consumption: Receiver<ConsumeResponse>,
+) -> Vec<GetIndexResponse> {
+    let mut return_vec = vec![];
+
+    while let Some(val) = consumption.recv().await {
+        let mut resp = GetIndexResponse {
+            records: val
+                .batches
+                .iter()
+                .map(|batch| {
+                    let stream_reader = read_arrow(&batch.data);
+
+                    let batches = stream_reader.filter_map(|val| val.ok()).collect::<Vec<_>>();
+
+                    let batch_refs = batches.iter().collect::<Vec<_>>();
+
+                    // Infer the batches
+                    let buf = Vec::new();
+                    let mut writer = arrow_json::LineDelimitedWriter::new(buf);
+                    writer.write_batches(&batch_refs).unwrap();
+                    writer.finish().unwrap();
+
+                    // Get the underlying buffer back,
+                    let buf = writer.into_inner();
+
+                    Record {
+                        data: buf,
+                        stream: batch.topic.as_bytes().to_vec(),
+                        offset: batch.offset,
+                        partition: batch.partition.clone(),
+                    }
+                })
+                .collect::<Vec<_>>(),
+        };
+
+        return_vec.push(resp);
+    }
+
+    return_vec
+}
--- higgins-core/src/utils/mod.rs
+++ higgins-core/src/utils/mod.rs
@@ -1,1 +1,2 @@
 pub mod request_response;
+pub mod consumption;
