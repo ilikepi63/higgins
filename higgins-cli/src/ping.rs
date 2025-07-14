3260c0a487bcbc636d8bf3edbd500d25fa1a4a23
Cameron <cameronh63@gmail.com>
2025-07-14 06:18:17 +0200

    feat: Server impl for timestamp + small fixes

--- higgins-cli/src/ping.rs
+++ higgins-cli/src/ping.rs
@@ -59,48 +59,6 @@ pub async fn handle_ping_cmd(socket: &mut TcpStream) {
 
             socket.write(&result).await.unwrap();
         }
-                Type::Createsubscriptionrequest => {
-                        tracing::info!("Received Consume Response!");
-                    }
-                    Type::Createsubscriptionresponse => todo!(),
-        Type::Producerequest => todo!(),
-        Type::Produceresponse => todo!(),
-        Type::Metadatarequest => todo!(),
-        Type::Metadataresponse => todo!(),
-        Type::Pong => {
-            tracing::info!("Received Pong!")
-        }
-        Type::Takerecordsrequest => todo!(),
-        Type::Takerecordsresponse => todo!(),
-        Type::Createconfigurationrequest => todo!(),
-        Type::Createconfigurationresponse => todo!(),
-        Type::Deleteconfigurationrequest => todo!(),
-        Type::Deleteconfigurationresponse => todo!(),
-    }
-
-    let result = socket.write_all(&write_buf).await.unwrap();
-    let n = socket.read(&mut read_buf).await.unwrap();
-
-    let slice = &read_buf[0..n];
-
-    let message = Message::decode(slice).unwrap();
-
-    match Type::try_from(message.r#type).unwrap() {
-        Type::Ping => {
-            let mut result = BytesMut::new();
-
-            let pong = Pong::default();
-
-            Message {
-                r#type: Type::Pong as i32,
-                pong: Some(pong),
-                ..Default::default()
-            }
-            .encode(&mut result)
-            .unwrap();
-
-            socket.write(&result).await.unwrap();
-        }
         Type::Createsubscriptionrequest => {
             tracing::info!("Received Consume Response!");
         }
@@ -117,6 +75,54 @@ pub async fn handle_ping_cmd(socket: &mut TcpStream) {
         Type::Createconfigurationrequest => todo!(),
         Type::Createconfigurationresponse => todo!(),
         Type::Deleteconfigurationrequest => todo!(),
+        Type::Getindexrequest => todo!(),
+        Type::Getindexresponse => todo!(),
+        Type::Error => todo!(),
         Type::Deleteconfigurationresponse => todo!(),
+    }
+
+    let result = socket.write_all(&write_buf).await.unwrap();
+    let n = socket.read(&mut read_buf).await.unwrap();
+
+    let slice = &read_buf[0..n];
+
+    let message = Message::decode(slice).unwrap();
+
+    match Type::try_from(message.r#type).unwrap() {
+        Type::Ping => {
+                        let mut result = BytesMut::new();
+        
+                        let pong = Pong::default();
+        
+                        Message {
+                            r#type: Type::Pong as i32,
+                            pong: Some(pong),
+                            ..Default::default()
+                        }
+                        .encode(&mut result)
+                        .unwrap();
+        
+                        socket.write(&result).await.unwrap();
+            }
+        Type::Createsubscriptionrequest => {
+                tracing::info!("Received Consume Response!");
+            }
+        Type::Createsubscriptionresponse => todo!(),
+        Type::Producerequest => todo!(),
+        Type::Produceresponse => todo!(),
+        Type::Metadatarequest => todo!(),
+        Type::Metadataresponse => todo!(),
+        Type::Pong => {
+                tracing::info!("Received Pong!")
+            }
+        Type::Takerecordsrequest => todo!(),
+        Type::Takerecordsresponse => todo!(),
+        Type::Createconfigurationrequest => todo!(),
+        Type::Createconfigurationresponse => todo!(),
+        Type::Deleteconfigurationrequest => todo!(),
+        Type::Deleteconfigurationresponse => todo!(),
+Type::Error => todo!(),
+        Type::Getindexrequest => todo!(),
+        Type::Getindexresponse => todo!(),
     }
 }
