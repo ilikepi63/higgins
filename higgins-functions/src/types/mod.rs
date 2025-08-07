2b0c3acfc0c75d269c567e855b49bf55da6a972e
Cameron <cameronh63@gmail.com>
2025-08-07 07:16:38 +0200

    fix: Converted from CDataInterface to IPC

--- higgins-functions/src/types/mod.rs
+++ higgins-functions/src/types/mod.rs
@@ -2,8 +2,10 @@ mod array;
 mod record_batch;
 mod schema;
 mod wasm_ptr;
+mod arbitrary_length_buffer;
 
 pub use array::WasmArrowArray;
 pub use record_batch::WasmRecordBatch;
 pub use schema::WasmArrowSchema;
 pub use wasm_ptr::WasmPtr;
+pub use arbitrary_length_buffer::ArbitraryLengthBuffer;
