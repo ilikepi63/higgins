dbc693d39b0a9a425de2f996408a0966c838a50b
Cameron <cameronh63@gmail.com>
2025-08-12 13:04:01 +0200

    feat: Initial Layout + Produce on Client

---
+++ higgins-client/src/lib.rs
@@ -0,0 +1,16 @@
+use bytes::BytesMut;
+use higgins_codec::frame::Frame;
+use higgins_codec::{Message, ProduceRequest, message::Type};
+use higgins_codec::{ProduceResponse, TakeRecordsRequest};
+use prost::Message as _;
+
+pub(crate) mod configuration;
+pub(crate) mod functions;
+pub(crate) mod ping;
+pub(crate) mod query;
+pub(crate) mod subscription;
+pub(crate) mod produce;
+
+mod client;
+
+pub use client::Client;
---
+++ higgins-client/src/subscription.rs
@@ -0,0 +1,104 @@
+use bytes::BytesMut;
+use higgins_codec::frame::Frame;
+use higgins_codec::{CreateSubscriptionRequest, TakeRecordsRequest};
+use higgins_codec::{Message, message::Type};
+use prost::Message as _;
+
+#[allow(unused)]
+pub fn create_subscription<T: std::io::Read + std::io::Write>(
+    stream: &[u8],
+    socket: &mut T,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let create_subscription = CreateSubscriptionRequest {
+        offset: None,
+        offset_type: 0,
+        timestamp: None,
+        stream_name: stream.to_vec(),
+    };
+
+    let mut write_buf = BytesMut::new();
+    let mut read_buf = BytesMut::zeroed(1024);
+
+    Message {
+        r#type: Type::Createsubscriptionrequest as i32,
+        create_subscription_request: Some(create_subscription),
+        ..Default::default()
+    }
+    .encode(&mut write_buf)?;
+
+    let frame = Frame::new(write_buf.to_vec());
+
+    frame.try_write(socket).unwrap();
+
+    let frame = Frame::try_read(socket).unwrap();
+
+    let slice = frame.inner();
+
+    let message = Message::decode(slice).unwrap();
+
+    let sub_id = match Type::try_from(message.r#type).unwrap() {
+        Type::Createsubscriptionresponse => {
+            let sub_id = message
+                .create_subscription_response
+                .unwrap()
+                .subscription_id;
+
+            sub_id.unwrap()
+        }
+        _ => panic!("Received incorrect response from server for Create Subscription request."),
+    };
+
+    Ok(sub_id)
+}
+
+
+#[allow(unused)]
+pub fn consume<T: std::io::Read + std::io::Write>(
+    sub_id: Vec<u8>,
+    stream_name: &[u8],
+    socket: &mut T,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    let take_request = TakeRecordsRequest {
+        n: 1,
+        subscription_id: sub_id,
+        stream_name: stream_name.to_vec(),
+    };
+
+    let mut write_buf = BytesMut::new();
+    let mut read_buf = BytesMut::zeroed(8048);
+
+    Message {
+        r#type: Type::Takerecordsrequest as i32,
+        take_records_request: Some(take_request),
+        ..Default::default()
+    }
+    .encode(&mut write_buf)
+    .unwrap();
+
+    let frame = Frame::new(write_buf.to_vec());
+
+    frame.try_write(socket).unwrap();
+
+    let frame = Frame::try_read(socket).unwrap();
+
+    let slice = frame.inner();
+
+    let message = Message::decode(slice).unwrap();
+
+    let result = match Type::try_from(message.r#type).unwrap() {
+        Type::Takerecordsresponse => {
+            tracing::info!("Receieved a take records response!");
+
+            let take_records_response = message.take_records_response.unwrap();
+
+            tracing::info!("Records_Response: {:#?}", take_records_response);
+
+            let record = take_records_response.records.iter().nth(0).unwrap();
+
+            record.data.clone()
+        }
+        _ => panic!("Received incorrect response from server for Create Subscription request."),
+    };
+
+    Ok(result)
+}
