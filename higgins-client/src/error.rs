b2e6ebbe3834dcf4eafa5012bc27b11355325e57
Cameron <cameronh63@gmail.com>
2025-08-13 00:00:00 +0200

    feat: Ping Adjustments

--- higgins-client/src/error.rs
+++ higgins-client/src/error.rs
@@ -1,7 +1,10 @@
+use higgins_codec::{errors::HigginsCodecError, message::Type};
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum HigginsClientError {
-    #[error("Unknown Error")]
-    Unknown,
+    #[error("Incorrect Response received. Expect: {0}, Received: {1}")]
+    IncorrectResponseReceived(String, String),
+    #[error("Higgins Codec Error")]
+    CodecError(#[from] HigginsCodecError),
 }
