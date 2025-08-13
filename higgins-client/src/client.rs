818aa2ca2105869c5bc9123e0266eb47c78cb2a0
Cameron <cameronh63@gmail.com>
2025-08-13 06:21:50 +0200

    feat: Updated Subscription and Added stubs to client

--- higgins-client/src/client.rs
+++ higgins-client/src/client.rs
@@ -1,34 +1,75 @@
 use crate::{
-    produce::produce_sync,
+    configuration::upload_configuration,
     error::HigginsClientError,
-    subscription::take
+    functions::upload_module,
+    ping::ping,
+    produce::produce_sync,
+    query::{query_by_timestamp, query_latest},
+    subscription::{create_subscription, take},
 };
-use higgins_codec::{ProduceResponse, TakeRecordsResponse};
+use higgins_codec::{CreateConfigurationResponse, ProduceResponse, Record, TakeRecordsResponse};
 
-pub struct Client (tokio::net::TcpStream);
+pub struct Client(tokio::net::TcpStream);
 
 impl Client {
-    pub async fn produce(&mut self, stream: &str, partition: &[u8], payload: &[u8]) -> Result<ProduceResponse, HigginsClientError> {
+    pub async fn produce(
+        &mut self,
+        stream: &str,
+        partition: &[u8],
+        payload: &[u8],
+    ) -> Result<ProduceResponse, HigginsClientError> {
         produce_sync(stream.as_bytes(), partition, payload, &mut self.0).await
     }
 
-    pub async fn take(&mut self,
+    pub async fn take(
+        &mut self,
         sub_id: Vec<u8>,
         stream_name: &[u8],
-        n: u64) -> Result<TakeRecordsResponse, HigginsClientError> {
+        n: u64,
+    ) -> Result<TakeRecordsResponse, HigginsClientError> {
         take(sub_id, stream_name, n, &mut self.0).await
     }
 
-    pub async fn ping(&self) {}
+    pub async fn ping(&mut self) -> Result<(), HigginsClientError> {
+        ping(&mut self.0).await
+    }
 
-    pub async fn query_by_timestamp(&self) {}
+    pub async fn query_by_timestamp(
+        &mut self,
+        stream: &[u8],
+        partition: &[u8],
+        timestamp: u64,
+    ) -> Result<Vec<Record>, HigginsClientError> {
+        query_by_timestamp(stream, partition, &mut self.0, timestamp).await
+    }
 
-    pub async fn query_latest(&self) {}
+    pub async fn query_latest(
+        &mut self,
+        stream: &[u8],
+        partition: &[u8],
+    ) -> Result<Vec<Record>, HigginsClientError> {
+        query_latest(stream, partition, &mut self.0).await
+    }
 
-    pub async fn create_subscription(&self) {}
+    pub async fn create_subscription(
+        &mut self,
+        stream: &[u8],
+    ) -> Result<Vec<u8>, HigginsClientError> {
+        create_subscription(stream, &mut self.0).await
+    }
 
-    pub async fn upload_module(&self) {}
+    pub async fn upload_module(
+        &mut self,
+        name: &str,
+        module: &[u8],
+    ) -> Result<(), HigginsClientError> {
+        upload_module(name, module, &mut self.0).await
+    }
 
-    pub async fn upload_configuration(&self) {}
-
+    pub async fn upload_configuration(
+        &mut self,
+        config: &[u8],
+    ) -> Result<CreateConfigurationResponse, HigginsClientError> {
+        upload_configuration(config, &mut self.0).await
+    }
 }
--- higgins-client/src/subscription.rs
+++ higgins-client/src/subscription.rs
@@ -1,14 +1,16 @@
+use crate::error::HigginsClientError;
 use bytes::BytesMut;
 use higgins_codec::frame::Frame;
-use crate::error::HigginsClientError;
 use higgins_codec::{CreateSubscriptionRequest, TakeRecordsRequest, TakeRecordsResponse};
 use higgins_codec::{Message, message::Type};
 use prost::Message as _;
 
-pub fn create_subscription<T: std::io::Read + std::io::Write>(
+pub async fn create_subscription<
+    T: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std::marker::Unpin,
+>(
     stream: &[u8],
     socket: &mut T,
-) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+) -> Result<Vec<u8>, HigginsClientError> {
     let create_subscription = CreateSubscriptionRequest {
         offset: None,
         offset_type: 0,
@@ -17,7 +19,6 @@ pub fn create_subscription<T: std::io::Read + std::io::Write>(
     };
 
     let mut write_buf = BytesMut::new();
-    let mut read_buf = BytesMut::zeroed(1024);
 
     Message {
         r#type: Type::Createsubscriptionrequest as i32,
@@ -28,27 +29,28 @@ pub fn create_subscription<T: std::io::Read + std::io::Write>(
 
     let frame = Frame::new(write_buf.to_vec());
 
-    frame.try_write(socket).unwrap();
+    frame.try_write_async(socket).await?;
 
-    let frame = Frame::try_read(socket).unwrap();
+    let frame = Frame::try_read_async(socket).await?;
 
     let slice = frame.inner();
 
     let message = Message::decode(slice).unwrap();
 
-    let sub_id = match Type::try_from(message.r#type).unwrap() {
+    match Type::try_from(message.r#type).unwrap() {
         Type::Createsubscriptionresponse => {
             let sub_id = message
                 .create_subscription_response
-                .unwrap()
+                .ok_or(HigginsClientError::MissingPayload)?
                 .subscription_id;
 
-            sub_id.unwrap()
+            Ok(sub_id.ok_or(HigginsClientError::MissingPayload)?)
         }
-        _ => panic!("Received incorrect response from server for Create Subscription request."),
-    };
-
-    Ok(sub_id)
+        _ => Err(HigginsClientError::IncorrectResponseReceived(
+            Type::Createsubscriptionresponse.as_str_name().to_string(),
+            message.r#type().as_str_name().to_string(),
+        )),
+    }
 }
 
 pub async fn take<T: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std::marker::Unpin>(
@@ -87,8 +89,7 @@ pub async fn take<T: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std::m
     let result = match Type::try_from(message.r#type).unwrap() {
         Type::Takerecordsresponse => {
             let take_records_response = message.take_records_response.unwrap();
-take_records_response
-
+            take_records_response
         }
         _ => panic!("Received incorrect response from server for Create Subscription request."),
     };
