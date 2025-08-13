1fa74c0ec54607271ae7e51a6c19f0b16e06419a
Cameron <cameronh63@gmail.com>
2025-08-13 06:07:36 +0200

    feat: Updated Modules

--- higgins-client/src/functions.rs
+++ higgins-client/src/functions.rs
@@ -3,11 +3,15 @@ use prost::Message as _;
 use bytes::BytesMut;
 use higgins_codec::{Message, UploadModuleRequest, frame::Frame, message::Type};
 
-pub fn upload_module<S: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std::marker::Unpin>(
+use crate::error::HigginsClientError;
+
+pub async fn upload_module<
+    S: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std::marker::Unpin,
+>(
     name: &str,
     wasm: &[u8],
     socket: &mut S,
-) {
+) -> Result<(), HigginsClientError> {
     let mut write_buf = BytesMut::new();
 
     let request = UploadModuleRequest {
@@ -20,34 +24,35 @@ pub fn upload_module<S: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std
         upload_module_request: Some(request),
         ..Default::default()
     }
-    .encode(&mut write_buf)
-    .unwrap();
+    .encode(&mut write_buf)?;
 
     let frame = Frame::new(write_buf.to_vec());
 
-    frame.try_write(socket).unwrap();
+    frame.try_write_async(socket).await?;
+
+    Ok(())
 }
 
-#[allow(unused)]
-pub fn upload_module_sync<
+pub async fn upload_module_sync<
     S: tokio::io::AsyncReadExt + tokio::io::AsyncWriteExt + std::marker::Unpin,
 >(
     name: &str,
     wasm: &[u8],
     socket: &mut S,
-) {
-    let mut read_buf = BytesMut::zeroed(20);
-
+) -> Result<(), HigginsClientError> {
     upload_module(name, wasm, socket);
 
-    let frame = Frame::try_read(socket).unwrap();
+    let frame = Frame::try_read_async(socket).await?;
 
     let slice = frame.inner();
 
     let message = Message::decode(slice).unwrap();
 
     match Type::try_from(message.r#type).unwrap() {
-        Type::Uploadmoduleresponse => {}
-        _ => panic!("Received incorrect response from server for ping request."),
+        Type::Uploadmoduleresponse => Ok(()),
+        _ => Err(HigginsClientError::IncorrectResponseReceived(
+            Type::Uploadmoduleresponse.as_str_name().to_string(),
+            message.r#type().as_str_name().to_string(),
+        )),
     }
 }
