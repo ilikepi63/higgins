8b24e7c780726390d9bd8e0932520a4bcc9b9d01
Cameron <cameronh63@gmail.com>
2025-09-07 00:00:00 +0200

    chore: Clean UP

--- higgins-core/src/broker/produce.rs
+++ higgins-core/src/broker/produce.rs
@@ -1,59 +1,11 @@
 use super::Broker;
 
-use arrow::{array::RecordBatch, datatypes::Schema};
-use bytes::BytesMut;
-use higgins_codec::{Message, Record, TakeRecordsResponse, message::Type};
-use prost::Message as _;
-use riskless::{
-    batch_coordinator::FindBatchResponse,
-    messages::{
-        ConsumeRequest, ConsumeResponse, ProduceRequest, ProduceRequestCollection, ProduceResponse,
-    },
-    object_store::{self, ObjectStore},
-};
-use std::{
-    collections::BTreeMap,
-    fs::create_dir,
-    path::PathBuf,
-    str::FromStr,
-    sync::{Arc, atomic::Ordering},
-    time::Duration,
-};
-use tokio::sync::{Notify, RwLock};
-use uuid::Uuid;
+use arrow::array::RecordBatch;
+use riskless::messages::{ProduceRequest, ProduceResponse};
 
 use crate::{
-    broker::object_store::path::Path,
-    derive::{
-        joining::create_joined_stream_from_definition, map::create_mapped_stream_from_definition,
-        reduce::create_reduced_stream_from_definition,
-    },
-    functions::collection::FunctionCollection,
-    topography::FunctionType,
+    error::HigginsError, storage::arrow_ipc::write_arrow, utils::request_response::Request,
 };
-use crate::{
-    client::ClientCollection,
-    error::HigginsError,
-    storage::{
-        arrow_ipc::{read_arrow, write_arrow},
-        indexes::IndexDirectory,
-    },
-    subscription::Subscription,
-    topography::{Topography, apply_configuration_to_topography, config::from_toml},
-    utils::request_response::Request,
-};
-use riskless::messages::ConsumeBatch;
-use std::collections::HashSet;
-
-type Receiver = tokio::sync::broadcast::Receiver<RecordBatch>;
-type Sender = tokio::sync::broadcast::Sender<RecordBatch>;
-
-type MutableCollection = Arc<
-    RwLock<(
-        ProduceRequestCollection,
-        Vec<Request<ProduceRequest, ProduceResponse>>,
-    )>,
->;
 
 impl Broker {
     /// Produce a data set onto the named stream.
