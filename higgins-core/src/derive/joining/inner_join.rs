0f95a666be830e9899622f73460e700953a6feba
Cameron <cameronh63@gmail.com>
2025-09-08 00:00:00 +0200

    fix: Typo on stream def

--- higgins-core/src/derive/joining/inner_join.rs
+++ higgins-core/src/derive/joining/inner_join.rs
@@ -6,7 +6,7 @@ pub struct InnerJoin {
     /// Name and definition of the stream that these streams are joined to.
     stream: (Key, StreamDefinition),
     /// The left (or derivative) stream that this stream is joined from.
-    left_strema: (Key, StreamDefinition),
+    left_stream: (Key, StreamDefinition),
     /// The right or outer stream that this stream is joined from.
     right_stream: (Key, StreamDefinition),
 }
