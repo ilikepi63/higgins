9b35770e4433553604a2271aacd4758f56890d6a
Cameron <cameronh63@gmail.com>
2025-09-08 00:00:00 +0200

    feat: Full Join definition

---
+++ higgins-core/src/derive/joining/full_join.rs
@@ -0,0 +1,13 @@
+/// A structure representing an Full Join.
+///
+/// This structure is primarily implemented for transporting full join
+/// configuration data.
+pub struct FullJoin {
+    /// Name and definition of the stream that these streams are joined to.
+    stream: (Key, StreamDefinition),
+    /// The first stream. There is no distinction between streams as a full join
+    /// does not differentiate between two streams.
+    first_stream: (Key, StreamDefinition),
+    /// The second stream definition.
+    second_stream: (Key, StreamDefinition),
+}
