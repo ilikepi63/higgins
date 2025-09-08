ed5bf7815c4f064cd77930328eb52fbdc73a04d0
Cameron <cameronh63@gmail.com>
2025-09-08 09:17:06 +0200

    feat: Implemented Inner Join Stream

---
+++ higgins-core/src/derive/joining/inner_join.rs
@@ -0,0 +1,12 @@
+/// A structure representing an Inner Join.
+///
+/// This structure is primarily implemented for transporting inner join
+/// configuration data.
+pub struct InnerJoin {
+    /// Name and definition of the stream that these streams are joined to.
+    stream: (Key, StreamDefinition),
+    /// The left (or derivative) stream that this stream is joined from.
+    left_strema: (Key, StreamDefinition),
+    /// The right or outer stream that this stream is joined from.
+    right_stream: (Key, StreamDefinition),
+}
