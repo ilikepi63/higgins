8169f08bff707c455e39c546219d5afa742a16e3
Cameron <cameronh63@gmail.com>
2025-09-16 00:00:00 +0200

    feat: Iterative Zerocopy in index files

--- higgins-core/src/storage/index/file.rs
+++ higgins-core/src/storage/index/file.rs
@@ -12,7 +12,7 @@ pub struct IndexFile {
 
 impl IndexFile {
     /// Create an instance from a path variable.
-    pub fn new(path: String) -> Result<Self, IndexError> {
+    pub fn new(path: &str) -> Result<Self, IndexError> {
         let file_handle = std::fs::OpenOptions::new()
             .read(true)
             .write(true)
@@ -24,9 +24,13 @@ impl IndexFile {
         let mmap = unsafe { MmapMut::map_mut(&file_handle)? };
 
         Ok(Self {
-            path,
+            path: path.to_owned(),
             file_handle,
             mmap,
         })
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        self.mmap.as_ref()
     }
 }
