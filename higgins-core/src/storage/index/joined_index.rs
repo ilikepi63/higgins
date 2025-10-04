4865f5011cd7d449c661e20d2b56eaf178057044
Cameron <cameronh63@gmail.com>
2025-10-05 00:00:00 +0200

    feat: Basic implementation of variable sized JoinIndex

--- higgins-core/src/storage/index/joined_index.rs
+++ higgins-core/src/storage/index/joined_index.rs
@@ -1,50 +1,41 @@
-use rkyv::{Archive, Serialize};
-use serde::Deserialize;
-
 use crate::storage::index::Timestamped;
 
 /// JoinedIndex represents the index metadata that one will use to
 /// keep track of both offsets of each stream this is derived from.
-#[derive(Serialize, Deserialize, Archive)]
-pub struct JoinedIndex {
-    /// The offset of the resultant index.
-    pub offset: u64,
-    /// The index of the right stream that this was joined from.
-    pub right_offset: Option<u64>,
-    /// The index of the left stream that this was joined from.
-    pub left_offset: Option<u64>,
-    /// The object key holding the resultant data from the joining.
-    pub object_key: Option<[u8; 16]>,
-    /// The timestamp for this index.
-    pub timestamp: u64,
+pub struct JoinedIndex<'a>(&'a [u8]);
+
+// /// The offset of the resultant index.
+// pub offset: u64,
+// /// The object key holding the resultant data from the joining.
+// pub object_key: Option<[u8; 16]>,
+// /// The timestamp for this index.
+// pub timestamp: u64,
+// /// The offsets of the derivative streams.
+// pub offsets: T,
+
+const OFFSET_INDEX: usize = 0;
+const OBJECT_KEY_INDEX: usize = OFFSET_INDEX + size_of::<u64>();
+const TIMESTAMP_INDEX: usize = OBJECT_KEY_INDEX + size_of::<Option<[u8; 16]>>();
+const INDEXES_INDEX: usize = TIMESTAMP_INDEX + size_of::<u64>();
+
+impl<'a> JoinedIndex<'a> {
+    // Properties.
+    /// Offset
+    pub fn offset(&self) -> u64 {
+        u64::from_be_bytes(self.0[OFFSET_INDEX..OBJECT_KEY_INDEX].try_into().unwrap())
+    }
+    pub fn object_key(&self) -> [u8; 16] {
+        self.0[OBJECT_KEY_INDEX..TIMESTAMP_INDEX]
+            .try_into()
+            .unwrap()
+    }
+    // pub fn offset(&self) -> u64 {
+    //     u64::from_be_bytes(self.0[OFFSET_INDEX..OBJECT_KEY_INDEX].try_into().unwrap())
+    // }
 }
 
-impl Timestamped for ArchivedJoinedIndex {
+impl<'a> Timestamped for JoinedIndex<'a> {
     fn timestamp(&self) -> u64 {
-        self.timestamp.to_native()
-    }
-}
-
-impl JoinedIndex {
-    /// Creates a new instance of this given a right_offset only.
-    pub fn new_with_right_offset(offset: u64, right_offset: u64, timestamp: u64) -> Self {
-        Self {
-            offset,
-            right_offset: Some(right_offset),
-            left_offset: None,
-            object_key: None,
-            timestamp,
-        }
-    }
-
-    /// Creates a new instance of this given a left_offset.
-    pub fn new_with_left_offset(offset: u64, left_offset: u64, timestamp: u64) -> Self {
-        Self {
-            offset,
-            right_offset: None,
-            left_offset: Some(left_offset),
-            object_key: None,
-            timestamp,
-        }
+        u64::from_be_bytes(self.0[TIMESTAMP_INDEX..INDEXES_INDEX].try_into().unwrap())
     }
 }
