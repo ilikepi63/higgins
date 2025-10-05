ca72996ba68debf1060a5c10c4d78d1b95c29511
Cameron <cameronh63@gmail.com>
2025-10-05 16:12:09 +0200

    feat: JoinedIndex constructor methods

--- higgins-core/src/storage/index/joined_index.rs
+++ higgins-core/src/storage/index/joined_index.rs
@@ -1,4 +1,5 @@
 use crate::storage::index::Timestamped;
+use std::io::Write;
 
 /// JoinedIndex represents the index metadata that one will use to
 /// keep track of both offsets of each stream this is derived from.
@@ -15,7 +16,7 @@ pub struct JoinedIndex<'a>(&'a [u8]);
 
 const OFFSET_INDEX: usize = 0;
 const OBJECT_KEY_INDEX: usize = OFFSET_INDEX + size_of::<u64>();
-const TIMESTAMP_INDEX: usize = OBJECT_KEY_INDEX + size_of::<Option<[u8; 16]>>();
+const TIMESTAMP_INDEX: usize = OBJECT_KEY_INDEX + size_of::<[u8; 16]>();
 const INDEXES_INDEX: usize = TIMESTAMP_INDEX + size_of::<u64>();
 
 impl<'a> JoinedIndex<'a> {
@@ -32,6 +33,31 @@ impl<'a> JoinedIndex<'a> {
     // pub fn offset(&self) -> u64 {
     //     u64::from_be_bytes(self.0[OFFSET_INDEX..OBJECT_KEY_INDEX].try_into().unwrap())
     // }
+    //
+    // Constructors
+    /// Puts the data into the mutable slice, returning this struct as a reference over it.
+    fn put(
+        &mut self,
+        offset: u64,
+        object_key: [u8; 16],
+        timestamp: u64,
+        indexes: &[u64],
+        mut data: &mut [u8],
+    ) {
+        data.write_all(offset.to_be_bytes().as_slice());
+        data.write_all(object_key.as_slice());
+        data.write_all(timestamp.to_be_bytes().as_slice());
+
+        for index in indexes {
+            data.write_all(index.to_be_bytes().as_slice());
+        }
+    }
+}
+
+impl<'a> From<&'a [u8]> for JoinedIndex<'a> {
+    fn from(src: &'a [u8]) -> Self {
+        Self(src)
+    }
 }
 
 impl<'a> Timestamped for JoinedIndex<'a> {
