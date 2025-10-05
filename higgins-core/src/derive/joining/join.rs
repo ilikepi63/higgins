ad88fb01c7a903da64aa10519f86225500f6ce46
Cameron <cameronh63@gmail.com>
2025-10-05 14:59:53 +0200

    feat: Remove conversion from stream definitions temporarily.

--- higgins-core/src/derive/joining/join.rs
+++ higgins-core/src/derive/joining/join.rs
@@ -13,56 +13,56 @@ pub struct JoinDefinition {
     joins: Vec<JoinWithStream>,
 }
 
-impl TryFrom<(Key, StreamDefinition, &Broker)> for JoinDefinition {
-    type Error = HigginsError;
+// impl TryFrom<(Key, StreamDefinition, &Broker)> for JoinDefinition {
+//     type Error = HigginsError;
 
-    fn try_from(
-        (key, stream_definition, broker): (Key, StreamDefinition, &Broker),
-    ) -> Result<Self, Self::Error> {
-        let left = broker
-            .get_topography_stream(&stream_definition.clone().base.unwrap())
-            .map(|(key, def)| (key.clone(), def.clone()))
-            .unwrap();
+//     fn try_from(
+//         (key, stream_definition, broker): (Key, StreamDefinition, &Broker),
+//     ) -> Result<Self, Self::Error> {
+//         let left = broker
+//             .get_topography_stream(&stream_definition.clone().base.unwrap())
+//             .map(|(key, def)| (key.clone(), def.clone()))
+//             .unwrap();
 
-        let right = broker
-            .get_topography_stream(
-                &stream_definition
-                    .clone()
-                    .join
-                    .map(|v| Key(v.key().to_owned()))
-                    .unwrap(),
-            )
-            .map(|(key, def)| (key.clone(), def.clone()))
-            .unwrap();
+//         let right = broker
+//             .get_topography_stream(
+//                 &stream_definition
+//                     .clone()
+//                     .join
+//                     .map(|v| Key(v.key().to_owned()))
+//                     .unwrap(),
+//             )
+//             .map(|(key, def)| (key.clone(), def.clone()))
+//             .unwrap();
 
-        Ok(match stream_definition.clone().join.unwrap() {
-            Join::Inner(_) => JoinDefinition::Inner(InnerJoin {
-                stream: (key, stream_definition),
-                left_stream: left,
-                right_stream: right,
-            }),
-            Join::LeftOuter(_) => JoinDefinition::Outer(OuterJoin {
-                side: super::outer_join::OuterSide::Left,
-                stream: (key, stream_definition),
-                left_stream: left,
-                right_stream: right,
-            }),
+//         Ok(match stream_definition.clone().join.unwrap() {
+//             Join::Inner(_) => JoinDefinition::Inner(InnerJoin {
+//                 stream: (key, stream_definition),
+//                 left_stream: left,
+//                 right_stream: right,
+//             }),
+//             Join::LeftOuter(_) => JoinDefinition::Outer(OuterJoin {
+//                 side: super::outer_join::OuterSide::Left,
+//                 stream: (key, stream_definition),
+//                 left_stream: left,
+//                 right_stream: right,
+//             }),
 
-            Join::RightOuter(_) => JoinDefinition::Outer(OuterJoin {
-                side: super::outer_join::OuterSide::Right,
-                stream: (key, stream_definition),
-                left_stream: left,
-                right_stream: right,
-            }),
+//             Join::RightOuter(_) => JoinDefinition::Outer(OuterJoin {
+//                 side: super::outer_join::OuterSide::Right,
+//                 stream: (key, stream_definition),
+//                 left_stream: left,
+//                 right_stream: right,
+//             }),
 
-            Join::Full(_) => JoinDefinition::Full(FullJoin {
-                stream: (key, stream_definition),
-                first_stream: left,
-                second_stream: right,
-            }),
-        })
-    }
-}
+//             Join::Full(_) => JoinDefinition::Full(FullJoin {
+//                 stream: (key, stream_definition),
+//                 first_stream: left,
+//                 second_stream: right,
+//             }),
+//         })
+//     }
+// }
 pub enum JoinWithStream {
     Full(FullJoin),
     Outer(OuterJoin),
