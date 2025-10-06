38b0ff7b91751be04b01f83d3e3cb68d9085c82c
Cameron <cameronh63@gmail.com>
2025-10-07 00:00:00 +0200

    feat: JoinedIndex fetch query helper

--- higgins-core/src/storage/index/joined_index.rs
+++ higgins-core/src/storage/index/joined_index.rs
@@ -1,4 +1,4 @@
-use crate::storage::index::{Timestamped, WrapBytes};
+use crate::storage::index::{IndexError, Timestamped, WrapBytes};
 use std::io::Write;
 
 /// JoinedIndex represents the index metadata that one will use to
@@ -36,7 +36,7 @@ impl<'a> JoinedIndex<'a> {
         offset: u64,
         object_key: Option<[u8; 16]>,
         timestamp: u64,
-        indexes: &[u64],
+        offsets: &[Option<u64>],
         mut data: &mut [u8],
     ) -> Result<(), std::io::Error> {
         data.write_all(offset.to_be_bytes().as_slice())?;
@@ -44,26 +44,66 @@ impl<'a> JoinedIndex<'a> {
 
         match object_key {
             Some(object_key) => {
-                data.write_all(&u8::to_be_bytes(1));
+                data.write_all(&u8::to_be_bytes(1))?;
                 data.write_all(object_key.as_slice())?;
             }
             None => {
-                data.write_all(&u8::to_be_bytes(0));
+                data.write_all(&u8::to_be_bytes(0))?;
                 data.write_all([0_u8; 16].as_slice())?;
             }
         }
 
-        for index in indexes {
-            data.write_all(index.to_be_bytes().as_slice())?;
+        for offset in offsets {
+            match offset {
+                Some(offset) => {
+                    data.write_all(&u8::to_be_bytes(1))?;
+                    data.write_all(offset.to_be_bytes().as_slice())?;
+                }
+                None => {
+                    data.write_all(&u8::to_be_bytes(0))?;
+                    // Write an empty byte array.
+                    data.write_all([0_u8; 8].as_slice())?;
+                }
+            }
         }
 
         Ok(())
     }
 
+    /// Gets the offset at the specified index.
+    pub fn get_offset(&self, index: usize) -> Result<u64, IndexError> {
+        match self.within_bounds(index) {
+            true => {
+                let relative_index = (index * (size_of::<u8>() + size_of::<u64>())) + INDEXES_INDEX;
+
+                let offset =
+                    &self.0[relative_index..relative_index + (size_of::<u8>() + size_of::<u64>())];
+
+                let (optional, offset) = offset.split_at(1);
+
+                if (u8::from_be_bytes(optional.try_into().unwrap()) == 1) {
+                    Ok(u64::from_be_bytes(offset.try_into().unwrap()))
+                } else {
+                    Err(IndexError::IndexInJoinedIndexNotFound)
+                }
+            }
+            false => Err(IndexError::IndexGivenOutOfBoundsForJoinedIndex),
+        }
+    }
+
+    /// Puts the offset at the specified index.
+    pub fn put_offset() {}
+
     // Helpers
     pub fn size_of(n_offsets: usize) -> usize {
         // last index (add one to make length), plus the amount of indexes times the size of the optional and the size of the offset.
         INDEXES_INDEX + 1 + (n_offsets * (size_of::<u8>() + size_of::<u64>()))
+    }
+
+    /// Checks whether an index given is within the specific bounds of this JoinedIndex.
+    fn within_bounds(&self, index: usize) -> bool {
+        index + INDEXES_INDEX < (self.0.len() - 1)
+            && (index + (size_of::<u8>() + size_of::<u64>())) < (self.0.len() - 1)
     }
 }
 
@@ -82,91 +122,5 @@ impl<'a> Timestamped for JoinedIndex<'a> {
 impl<'a> WrapBytes<'a> for JoinedIndex<'a> {
     fn wrap(bytes: &'a [u8]) -> Self {
         Self(bytes)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_from_slice() {
-        let data = vec![0u8; INDEXES_INDEX];
-        let index = JoinedIndex::from(&data[..]);
-        assert_eq!(index.0, data.as_slice());
-    }
-
-    #[test]
-    fn test_offset() {
-        let mut data = vec![0u8; INDEXES_INDEX];
-        let expected_offset: u64 = 0x123456789ABCDEF0;
-        data[OFFSET_INDEX..OBJECT_KEY_INDEX].copy_from_slice(&expected_offset.to_be_bytes());
-
-        let index = JoinedIndex::from(&data[..]);
-        assert_eq!(index.offset(), expected_offset);
-    }
-
-    #[test]
-    fn test_object_key() {
-        let mut data = vec![0u8; INDEXES_INDEX];
-        let expected_key = [0xAA; 16];
-        data[OBJECT_KEY_INDEX..TIMESTAMP_INDEX].copy_from_slice(&expected_key);
-
-        let index = JoinedIndex::from(&data[..]);
-        assert_eq!(index.object_key(), expected_key);
-    }
-
-    #[test]
-    fn test_timestamp() {
-        let mut data = vec![0u8; INDEXES_INDEX];
-        let expected_timestamp: u64 = 0xFEDCBA9876543210;
-        data[TIMESTAMP_INDEX..INDEXES_INDEX].copy_from_slice(&expected_timestamp.to_be_bytes());
-
-        let index = JoinedIndex::from(&data[..]);
-        assert_eq!(index.timestamp(), expected_timestamp);
-    }
-
-    #[test]
-    fn test_timestamped_trait() {
-        let mut data = vec![0u8; INDEXES_INDEX];
-        let expected_timestamp: u64 = 0xFEDCBA9876543210;
-        data[TIMESTAMP_INDEX..INDEXES_INDEX].copy_from_slice(&expected_timestamp.to_be_bytes());
-
-        let index = JoinedIndex::from(&data[..]);
-        assert_eq!(Timestamped::timestamp(&index), expected_timestamp);
-    }
-
-    #[test]
-    fn test_put_returns_error_on_short_data() {
-        let mut index = JoinedIndex::from(&[0u8; 0][..]); // Empty slice for mut self
-        let mut short_data = vec![0u8; 10]; // Too short
-        let result = index.put(0, Some([0u8; 16]), 0, &[], &mut short_data);
-
-        assert!(matches!(result, Err(_)));
-    }
-
-    #[test]
-    fn test_put_writes_correctly() {
-        let mut index = JoinedIndex::from(&[0u8; 32][..]); // Dummy for mut self, ignored
-        let offset: u64 = 0x123456789ABCDEF0;
-        let object_key = [0xAA; 16];
-        let timestamp: u64 = 0xFEDCBA9876543210;
-        let indexes = vec![0x1111111111111111u64, 0x2222222222222222u64];
-
-        let mut data = vec![0u8; 32 + indexes.len() * 8];
-        index.put(offset, Some(object_key), timestamp, &indexes, &mut data);
-
-        // Verify written data
-        let read_offset = u64::from_be_bytes(data[0..8].try_into().unwrap());
-        let read_key: [u8; 16] = data[8..24].try_into().unwrap();
-        let read_timestamp = u64::from_be_bytes(data[24..32].try_into().unwrap());
-        let read_index1 = u64::from_be_bytes(data[32..40].try_into().unwrap());
-        let read_index2 = u64::from_be_bytes(data[40..48].try_into().unwrap());
-
-        assert_eq!(read_offset, offset);
-        assert_eq!(read_key, object_key);
-        assert_eq!(read_timestamp, timestamp);
-        assert_eq!(read_index1, indexes[0]);
-        assert_eq!(read_index2, indexes[1]);
     }
 }
