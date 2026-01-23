618c38918cfe201e735e5077669cd5088cc1cc0f
Cameron <cameron.harris@ingka.com>
2026-01-23 14:07:54 +0100

    fix: Small adjustment to CLI

--- higgins-cli/src/main.rs
+++ higgins-cli/src/main.rs
@@ -18,7 +18,7 @@ mod ping;
 #[command(version, about, long_about = None)]
 struct Args {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
@@ -59,74 +59,70 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     match args.command {
-        Some(Commands::Ping {}) => {
+        Commands::Ping {} => {
             handle_ping_cmd(&mut socket).await;
         }
-        Some(cmd) if matches!(&cmd, Commands::Produce { .. }) => {
-            if let Commands::Produce {
-                topic,
-                key,
-                file_name,
-            } = cmd
-            {
-                let data = std::fs::read_to_string(&file_name).unwrap();
+        Commands::Produce {
+            topic,
+            key,
+            file_name,
+        } => {
+            let data = std::fs::read_to_string(&file_name).unwrap();
 
-                let request = ProduceRequest {
-                    stream_name: topic.as_bytes().to_vec(),
-                    partition_key: key,
-                    payload: data.as_bytes().to_vec(),
-                };
+            let request = ProduceRequest {
+                stream_name: topic.as_bytes().to_vec(),
+                partition_key: key,
+                payload: data.as_bytes().to_vec(),
+            };
 
-                let mut write_buf = BytesMut::new();
-                let mut read_buf = BytesMut::new();
+            let mut write_buf = BytesMut::new();
+            let mut read_buf = BytesMut::new();
 
-                Message {
-                    r#type: Type::Producerequest as i32,
-                    produce_request: Some(request),
-                    ..Default::default()
+            Message {
+                r#type: Type::Producerequest as i32,
+                produce_request: Some(request),
+                ..Default::default()
+            }
+            .encode(&mut write_buf)
+            .unwrap();
+
+            tracing::info!("Writing: {:#?}", write_buf);
+
+            socket.write_all(&write_buf).await.unwrap();
+
+            let n = socket.read(&mut read_buf).await.unwrap();
+
+            let slice = &read_buf[0..n];
+
+            let message = Message::decode(slice).unwrap();
+
+            match Type::try_from(message.r#type).unwrap() {
+                Type::Ping => {}
+                Type::Createsubscriptionrequest => {
+                    tracing::info!("Received Consume Response!");
                 }
-                .encode(&mut write_buf)
-                .unwrap();
-
-                tracing::info!("Writing: {:#?}", write_buf);
-
-                socket.write_all(&write_buf).await.unwrap();
-
-                let n = socket.read(&mut read_buf).await.unwrap();
-
-                let slice = &read_buf[0..n];
-
-                let message = Message::decode(slice).unwrap();
-
-                match Type::try_from(message.r#type).unwrap() {
-                    Type::Ping => {}
-                    Type::Createsubscriptionrequest => {
-                        tracing::info!("Received Consume Response!");
-                    }
-                    Type::Createsubscriptionresponse => todo!(),
-                    Type::Producerequest => {}
-                    Type::Produceresponse => {
-                        tracing::info!("Received Produce Response!");
-                    }
-                    Type::Metadatarequest => todo!(),
-                    Type::Metadataresponse => todo!(),
-                    Type::Pong => {}
-                    Type::Takerecordsrequest => todo!(),
-                    Type::Takerecordsresponse => todo!(),
-                    Type::Createconfigurationrequest => todo!(),
-                    Type::Createconfigurationresponse => todo!(),
-                    Type::Deleteconfigurationrequest => todo!(),
-                    Type::Deleteconfigurationresponse => todo!(),
-                    Type::Getindexrequest => todo!(),
-                    Type::Getindexresponse => todo!(),
-                    Type::Uploadmodulerequest => todo!(),
-                    Type::Uploadmoduleresponse => todo!(),
-                    Type::Error => todo!(),
+                Type::Createsubscriptionresponse => todo!(),
+                Type::Producerequest => {}
+                Type::Produceresponse => {
+                    tracing::info!("Received Produce Response!");
                 }
+                Type::Metadatarequest => todo!(),
+                Type::Metadataresponse => todo!(),
+                Type::Pong => {}
+                Type::Takerecordsrequest => todo!(),
+                Type::Takerecordsresponse => todo!(),
+                Type::Createconfigurationrequest => todo!(),
+                Type::Createconfigurationresponse => todo!(),
+                Type::Deleteconfigurationrequest => todo!(),
+                Type::Deleteconfigurationresponse => todo!(),
+                Type::Getindexrequest => todo!(),
+                Type::Getindexresponse => todo!(),
+                Type::Uploadmodulerequest => todo!(),
+                Type::Uploadmoduleresponse => todo!(),
+                Type::Error => todo!(),
             }
         }
-        Some(_) => todo!(),
-        None => todo!(),
+        _ => todo!(),
     }
 
     Ok(())
