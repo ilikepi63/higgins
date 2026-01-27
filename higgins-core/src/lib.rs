617b0b2b4c9cd1988274c8550ccfb927a8163291
Cameron <cameron.harris@ingka.com>
2026-01-27 14:28:30 +0100

    feat: Rewrite for GetIndexRequest

--- higgins-core/src/lib.rs
+++ higgins-core/src/lib.rs
@@ -2,10 +2,8 @@ use std::{path::PathBuf, sync::Arc};
 
 use bytes::BytesMut;
 use higgins_codec::{
-    CreateConfigurationRequest, CreateConfigurationResponse, Error, GetIndexResponse, Message,
-    Record, UploadModuleRequest, UploadModuleResponse, frame::Frame, message::Type,
+    Message, UploadModuleRequest, UploadModuleResponse, frame::Frame, message::Type,
 };
-use higgins_shared::PartitionName;
 use prost::Message as _;
 use task::SpawnTaskConfig;
 use tokio::{
@@ -14,7 +12,7 @@ use tokio::{
     sync::RwLock,
 };
 
-use crate::{broker::Broker, client::ClientRef, storage::arrow_ipc::read_arrow};
+use crate::{broker::Broker, client::ClientRef};
 pub mod broker;
 pub mod client;
 mod derive;
@@ -110,136 +108,8 @@ async fn process_socket(tcp_socket: TcpStream, broker: Arc<RwLock<Broker>>) {
                     }
                     Type::Error => {}
                     Type::Getindexrequest => {
-                        tracing::trace!("Trying to retrieve the broker lock..");
-
-                        let mut broker_lock = broker.write().await;
-
-                        tracing::trace!("Retrieved the GetIndexRequest");
-
-                        let request = message.get_index_request.unwrap(); // TODO: error response here.
-                        tracing::trace!("Retrieved the GetIndexRequest: {:#?}", request);
-
-                        for index in request.indexes {
-                            // We can potentially query in three different ways using this request, so
-                            // this match arm reflects that.
-                            match index.r#type() {
-                                higgins_codec::index::Type::Timestamp => {
-                                    let values = broker_lock
-                                        .get_by_timestamp(
-                                            &index.stream,
-                                            &PartitionName::try_from(&index.partition[..]).unwrap(),
-                                            index.timestamp(),
-                                        )
-                                        .await
-                                        .unwrap();
-
-                                    let response = GetIndexResponse {
-                                        records: values
-                                            .batches
-                                            .iter()
-                                            .map(|batch| {
-                                                let stream_reader = read_arrow(&batch.data);
-
-                                                let batches = stream_reader
-                                                    .filter_map(|val| val.ok())
-                                                    .collect::<Vec<_>>();
-
-                                                let batch_refs = batches.iter().collect::<Vec<_>>();
-
-                                                // Infer the batches
-                                                let buf = Vec::new();
-                                                let mut writer =
-                                                    arrow_json::LineDelimitedWriter::new(buf);
-                                                writer.write_batches(&batch_refs).unwrap();
-                                                writer.finish().unwrap();
-
-                                                // Get the underlying buffer back,
-                                                let buf = writer.into_inner();
-
-                                                Record {
-                                                    data: buf,
-                                                    stream: batch.topic.as_bytes().to_vec(),
-                                                    offset: batch.offset,
-                                                    partition: batch.partition.clone(),
-                                                }
-                                            })
-                                            .collect::<Vec<_>>(),
-                                    };
-
-                                    // let responses = collect_consume_responses(values).await;
-
-                                    // for response in responses {
-                                    let mut result = BytesMut::new();
-
-                                    Message {
-                                        r#type: Type::Getindexresponse as i32,
-                                        get_index_response: Some(response),
-                                        ..Default::default()
-                                    }
-                                    .encode(&mut result)
-                                    .unwrap();
-
-                                    writer_tx.send(result).await.unwrap();
-                                    // }
-                                }
-                                higgins_codec::index::Type::Latest => {
-                                    tracing::trace!("Retrieved a Latest GetIndexRequest",);
-
-                                    let partition =
-                                        &PartitionName::try_from(&index.partition[..]).unwrap();
-
-                                    let responses =
-                                        broker_lock.get_latest(&index.stream, partition).await;
-
-                                    for response in responses {
-                                        let response = response.await.unwrap();
-
-                                        tracing::trace!(
-                                            "Response for GetIndexRequest: {:#?}",
-                                            response
-                                        );
-
-                                        let index_response = GetIndexResponse {
-                                            records: vec![Record {
-                                                data: response,
-                                                stream: vec![],
-                                                partition: vec![],
-                                                offset: 0,
-                                            }],
-                                        };
-
-                                        let mut result = BytesMut::new();
-
-                                        Message {
-                                            r#type: Type::Getindexresponse as i32,
-                                            get_index_response: Some(index_response),
-                                            ..Default::default()
-                                        }
-                                        .encode(&mut result)
-                                        .unwrap();
-
-                                        writer_tx.send(result).await.unwrap();
-                                    }
-                                }
-                                higgins_codec::index::Type::Offset => {
-                                    let mut result = BytesMut::new();
-
-                                    let mut error = Error::default();
-
-                                    error.set_type(higgins_codec::error::Type::Unimplemented);
-
-                                    Message {
-                                        r#type: Type::Error as i32,
-                                        error: Some(error),
-                                        ..Default::default()
-                                    }
-                                    .encode(&mut result)
-                                    .unwrap();
-
-                                    writer_tx.send(result).await.unwrap();
-                                }
-                            }
-                        }
+                        handlers::handle_get_index(message, broker.clone(), writer_tx.clone())
+                            .await;
                     }
                     Type::Getindexresponse => {}
                     Type::Uploadmodulerequest => {
