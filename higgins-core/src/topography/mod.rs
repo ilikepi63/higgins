6a260a4e97e77ebe3a1afc7e1b0e154a59054714
Cameron <cameron.harris@ingka.com>
2026-02-05 16:02:54 +0100

    fix: Integration tests failing for topography file

--- higgins-core/src/topography/mod.rs
+++ higgins-core/src/topography/mod.rs
@@ -74,28 +74,40 @@ impl Topography {
     pub fn from_file(file: std::path::PathBuf) -> Result<Self, TopographyError> {
         let file = TopographyFile::new(file);
 
-        let operations: Vec<TopographyUnit> = file.read()?;
+        let (streams, schema, storage) = match file.read() {
+            Ok(operations) => {
 
-        let (streams, schema, storage) = {
-            operations
-                .iter()
-                .fold((BTreeMap::new(), BTreeMap::new(), None), |mut acc, unit| {
-                    match unit {
-                        TopographyUnit::Stream((key, stream)) => {
-                            acc.0.insert(Key(key.as_bytes().to_owned()), stream.clone());
-                        }
-                        TopographyUnit::Schema((key, schema)) => {
-                            acc.1
-                                .insert(Key(key.as_bytes().to_owned()), Arc::new(schema.clone()));
-                        }
-                        TopographyUnit::Storage((key, storage)) => {
-                            acc.2 = Some((key.to_owned(), storage.clone()))
-                        }
-                    };
+                    Ok(operations
+                        .iter()
+                        .fold((BTreeMap::new(), BTreeMap::new(), None), |mut acc, unit| {
+                            match unit {
+                                TopographyUnit::Stream((key, stream)) => {
+                                    acc.0.insert(Key(key.as_bytes().to_owned()), stream.clone());
+                                }
+                                TopographyUnit::Schema((key, schema)) => {
+                                    acc.1
+                                        .insert(Key(key.as_bytes().to_owned()), Arc::new(schema.clone()));
+                                }
+                                TopographyUnit::Storage((key, storage)) => {
+                                    acc.2 = Some((key.to_owned(), storage.clone()))
+                                }
+                            };
 
-                    acc
-                })
-        };
+                            acc
+                        }))
+            },
+            Err(err) => {
+
+                if let TopographyError:: IOError(err) = &err && err.kind() == std::io::ErrorKind::NotFound {
+                    Ok((BTreeMap::new(), BTreeMap::new(), None))
+                }else {
+                    Err(err)
+                }
+            }
+        }?;
+
+        // let operations: Vec<TopographyUnit> = file.read();
+
 
         Ok(Self {
             file,
