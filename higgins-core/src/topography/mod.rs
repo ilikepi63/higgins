e3abced8397d01e98ced0d3921f3be965adbb8a6
Cameron <cameron.harris@ingka.com>
2026-02-09 09:28:23 +0100

    fix: clean up for files

--- higgins-core/src/topography/mod.rs
+++ higgins-core/src/topography/mod.rs
@@ -447,9 +447,9 @@ pub struct SubscriptionDeclaration {
 #[cfg(test)]
 pub mod test {
 
-    use crate::topography::config::from_toml;
-
     use super::*;
+    use crate::topography::config::from_toml;
+    use std::panic::catch_unwind;
 
     static BASIC_CONFIG: &str = r#"
     [storage.s3]
@@ -475,16 +475,24 @@ pub mod test {
     pub fn can_convert_topography_to_configuration() {
         let file_name = std::path::PathBuf::from(uuid::Uuid::new_v4().to_string());
 
-        let config = from_toml(BASIC_CONFIG.as_bytes());
+        let destroy_file_name = file_name.clone();
 
-        let mut topography = Topography::from_file(file_name).unwrap();
+        let result = catch_unwind(|| {
+            let config = from_toml(BASIC_CONFIG.as_bytes());
 
-        topography
-            .apply_configuration_to_topography(&config)
-            .unwrap();
+            let mut topography = Topography::from_file(file_name).unwrap();
 
-        let config_from_topography = topography.to_config();
+            topography
+                .apply_configuration_to_topography(&config)
+                .unwrap();
 
-        assert_eq!(config, config_from_topography);
+            let config_from_topography = topography.to_config();
+
+            assert_eq!(config, config_from_topography);
+        });
+
+        std::fs::remove_dir_all(destroy_file_name).unwrap();
+
+        result.unwrap();
     }
 }
