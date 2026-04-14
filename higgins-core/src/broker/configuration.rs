04750ebc5fb7d96c28751a4dc6d201c17d3f068d
Cameron <cameron.harris@ingka.com>
2026-04-14 10:21:29 +0200

    feat: Change configuration to handle creation of windowing function

--- higgins-core/src/broker/configuration.rs
+++ higgins-core/src/broker/configuration.rs
@@ -1,5 +1,6 @@
 use super::Broker;
 use crate::derive::joining::{create_joined_stream_from_definition, join::JoinDefinition};
+use crate::derive::windowed::create_windowed_stream_from_definition;
 use crate::storage::backing_store::{BackingStore, ObjectBackingStore};
 use crate::topography::config::{Storage, StorageType};
 use object_store::aws::AmazonS3Builder;
@@ -140,6 +141,16 @@ impl Broker {
                     )
                     .await
                     .unwrap();
+                }
+                Some(FunctionType::Window) => {
+                    tracing::trace!("Creating Windowed stream from stream definition.");
+
+                    create_windowed_stream_from_definition(
+                        derived_stream_definition,
+                        self,
+                        broker.clone(),
+                    )
+                    .await;
                 }
                 Some(_) => todo!(),
                 None => {
