40c66410165ec8904efb5ca7fc91d4a6c35b9717
Cameron Harris <cameronh63@gmail.com>
2026-05-19 08:23:35 +0200

    chore: Add failing test for windowed stream

--- higgins-core/tests/basic_windowing.rs
+++ higgins-core/tests/basic_windowing.rs
@@ -4,7 +4,7 @@ use common::{
     configuration::upload_configuration_sync, ping::client_sync_ping_test, schema::customer_schema,
 };
 use higgins::run_server;
-use higgins_client::ResponseBody;
+use higgins_client::{Response, ResponseBody};
 use higgins_shared::{PartitionName, read_arrow};
 use std::{panic::catch_unwind, path::PathBuf, time::Duration};
 
@@ -17,8 +17,15 @@ fn get_dir() -> PathBuf {
     dir
 }
 
-static STREAM: &str = "update_customer";
-static PARTITION: &[u8] = "1".as_bytes();
+static STREAM: &str = "value";
+static WINDOWED_STREAM: &str = "value_windowed";
+
+static PAYLOAD: &str = r#"{
+    "id": "1",
+    "some_data": "1",
+    "other_data": "1"
+    "i": 123
+}"#;
 
 #[test]
 fn basic_windowing() {
@@ -51,9 +58,66 @@ fn basic_windowing() {
             &mut client,
         );
         tracing::info!("Uploaded the config..");
+
+        client
+            .produce_json(
+                STREAM,
+                PAYLOAD.as_bytes(),
+                std::sync::Arc::new(value_schema()),
+            )
+            .unwrap();
+
+        let produce_result = client.recv(Some(Duration::from_secs(1)));
+
+        assert!(matches!(
+            produce_result.unwrap().body,
+            ResponseBody::Produce(_)
+        ));
+
+        client
+            .query_at(
+                WINDOWED_STREAM.as_bytes(),
+                &PartitionName::try_from("1").unwrap(),
+                0,
+            )
+            .unwrap();
+
+        let response = client.recv(Some(Duration::from_secs(1))).unwrap();
+
+        match response.body {
+            ResponseBody::GetIndex(index_data) => {
+                let record = index_data.records.first().unwrap();
+
+                assert_eq!(record.offset, 0);
+                assert_eq!(record.partition, "1".as_bytes());
+                assert_eq!(record.stream, WINDOWED_STREAM.as_bytes());
+
+                let arrow = read_arrow(&record.data)
+                    .next()
+                    .unwrap()
+                    .inspect_err(|err| {
+                        dbg!(err);
+                    })
+                    .unwrap();
+
+                tracing::debug!("{:#?}", arrow);
+            }
+            _ => panic!("Retreved incorrect response for produce query."),
+        }
     });
 
     std::fs::remove_dir_all(dir_remove).unwrap();
 
     result.unwrap();
+}
+
+use arrow_schema::{DataType, Field, Schema};
+
+pub fn value_schema() -> Schema {
+    Schema::new(vec![
+        Field::new("id", arrow_schema::DataType::Utf8, false),
+        Field::new("some_data", arrow_schema::DataType::Utf8, false),
+        Field::new("other_data", DataType::Utf8, false),
+        Field::new("i", DataType::Int32, false),
+    ])
 }
