ca91d82eb0194dcbe4ded03c8bd824f5a57d8606
Cameron Harris <cameronh63@gmail.com>
2026-05-25 20:56:26 +0200

    fix: small changes

--- higgins-core/src/derive/map.rs
+++ higgins-core/src/derive/map.rs
@@ -1,78 +1,73 @@
-use std::sync::Arc;
-use tokio::sync::RwLock;
-
 use crate::{
     broker::Broker,
-    derive::utils::get_partition_key_from_record_batch,
+    derive::{joining::opts::eager_range_take_or_wait, utils::get_partition_key_from_record_batch},
     error::HigginsError,
     functions::map::run_map_function,
     topography::{Key, StreamDefinition},
 };
 use higgins_shared::{PartitionName, read_arrow};
+use std::sync::Arc;
+use tokio::sync::RwLock;
 
 pub async fn create_mapped_stream_from_definition(
     stream_name: Key,
     stream_def: StreamDefinition,
-    left: (Key, StreamDefinition),
+    base_stream: (Key, StreamDefinition),
     broker: &mut Broker,
     broker_ref: Arc<RwLock<Broker>>,
 ) -> Result<(), HigginsError> {
     // Subscribe to both streams.
-    let left_subscription = broker.create_subscription(left.0.as_bytes());
+    // let left_subscription = broker.create_subscription(left.0.as_bytes());
 
-    let (left_notify, left_subscription_ref) = broker
-        .get_subscription_by_key(left.0.as_bytes(), &left_subscription)
-        .unwrap();
-    let (_right_notify, _right_subscription_ref) = broker
-        .get_subscription_by_key(left.0.as_bytes(), &left_subscription)
-        .unwrap();
+    let (client_id, condvar, subscription) = {
+        tracing::trace!("Attempting to input client_id.");
 
-    let left_broker = broker_ref.clone();
-    let left_stream_name = left.0.as_bytes().to_owned();
-    let left_stream_partition_key = left.1.partition_key;
+        let client_id = broker
+            .clients
+            .insert(crate::client::ClientRef::NoOp)
+            .unwrap();
+
+        tracing::trace!("Retrieved client_id.");
+        let subscription = broker.create_subscription(base_stream.0.as_bytes());
+
+        tracing::trace!("Successfully created the subscription.");
+
+        let (notify, subscription) = broker
+            .get_subscription_by_key(base_stream.0.as_bytes(), &subscription)
+            .ok_or(HigginsError::SubscriptionRetrievalFailed)
+            .unwrap();
+
+        tracing::trace!("Retrieved the notification for said subscription.");
+
+        (client_id, notify, subscription)
+    };
 
     // Left join runner for this subscription.
     tokio::task::spawn(async move {
         tracing::trace!("[DERIVED TAKE] We are being initiated");
 
         loop {
-            let mut lock = left_subscription_ref.write().await;
+            let offsets =
+                eager_range_take_or_wait(subscription.clone(), condvar.clone(), client_id)
+                    .await
+                    .unwrap();
+
+            let mut lock = subscription.write().await;
 
             let n = 10; // Generally, there is a set amount of n that we are interested in at a point.
 
-            let offsets_result = lock.take(n);
+            let offsets_result = lock.take_range(n);
 
             drop(lock);
 
             if let Ok(mut offsets) = offsets_result {
-                // If there are no given offsts, await the wakener then.
-                if offsets.is_empty() {
-                    tracing::trace!("[DERIVED TAKE] Awaiting to be notified for produce..");
-                    left_notify.notified().await;
-                    tracing::trace!("[DERIVED TAKE] We've been notified!");
-
-                    offsets = {
-                        let mut lock = left_subscription_ref.write().await;
-                        lock.take(n).unwrap()
-                    };
-                }
-
-                tracing::trace!(
-                    "[DERIVED TAKE] Received offsets {:#?}. Initiating join.",
-                    offsets
-                );
-
                 //Get payloads from offsets.
                 for (partition, offset) in offsets {
-                    let mut broker_lock = left_broker.write().await;
-
-                    let consumption = broker_lock
-                        .consume(&left_stream_name, &partition, offset, 50_000)
-                        .await;
+                    let mut broker_lock = broker_ref.write().await;
 
                     let mut records = vec![];
 
-                    for val in consumption {
+                    for offset in (offset.start..=offset.end) {
                         let val = val.unwrap();
                         records.push(val);
                     }
