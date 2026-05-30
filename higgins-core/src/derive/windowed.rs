468db0f8c4608d6734c3600ddc6b15ca2a1a8952
Cameron Harris <cameronh63@gmail.com>
2026-05-30 20:28:13 +0200

    chore(315): Refactor Window operation

--- higgins-core/src/derive/windowed.rs
+++ higgins-core/src/derive/windowed.rs
@@ -6,13 +6,115 @@ use crate::derive::windowed::definition::WindowValue;
 use crate::error::HigginsError;
 use crate::storage::index::file::windowed_index_file::WindowedIndexFile;
 use crate::storage::windowing::assign_sliding_windows_range;
+use crate::subscription::Subscription;
 use crate::task::SpawnTaskConfig;
 use definition::WindowedStreamDefinition;
 use higgins_shared::PartitionName;
+use std::ops::Range;
 use std::sync::Arc;
 use tokio::sync::RwLock;
 
 pub mod definition;
+
+pub struct WindowOperation {
+    /// Broker  Reference.
+    broker: Arc<RwLock<Broker>>,
+    /// This resultant stream's stream name.
+    stream: String,
+    /// This resultant streams stream definition.
+    definition: WindowedStreamDefinition,
+    /// The partition we've received offsets on.
+    partition: PartitionName,
+    /// The offsets.
+    offsets: Range<u64>,
+    // /// The references - We want to use these to commit so we have to save them over init and commit branches.
+    // references: Option<Vec<Reference>>,
+    /// The subscription that controls how this stream is tracked.
+    subscription: Arc<RwLock<Subscription>>,
+    // The underlying records that this operation is based on. (Current unused )
+    // Vec<(
+    //   Vec<u8> - IPC record batch.
+    //   u64 - The offset to which it belongs.
+    // )>
+    // records: Vec<(Vec<u8>, u64)>,
+}
+
+impl WindowOperation {
+    pub async fn init(&mut self) -> Result<(), HigginsError> {
+        Ok(())
+    }
+    pub async fn prepare(&mut self) -> Result<(), HigginsError> {
+        Ok(())
+    }
+    pub async fn commit(&mut self) -> Result<(), HigginsError> {
+        match &self.definition.window_type {
+            WindowValue::Count(count) => {
+                let resultant_stream = String::from_utf8(self.stream.as_bytes().to_vec()).unwrap();
+
+                tracing::info!("Retrieving index file for stream {resultant_stream}");
+
+                // TODO: maybe paralellize these?
+                let mut resultant_index_file =
+                    get_index_file_handle(&resultant_stream, &self.partition, self.broker.clone())
+                        .await;
+
+                tracing::info!("Retrieved index file..");
+
+                let mut new_ranges = assign_sliding_windows_range(
+                    self.offsets.clone(),
+                    count.clone(),
+                    self.definition.slide.normalize(),
+                    0,
+                );
+
+                let mut guard = resultant_index_file.lock().await;
+                let index_file = guard.as_index();
+
+                let mut windowed_index_file = WindowedIndexFile::of(index_file);
+
+                tracing::info!("Applying ranges {:#?} to windowed index file.", new_ranges);
+
+                windowed_index_file.put_ranges(&mut new_ranges).unwrap();
+
+                // acknowledge me!
+                {
+                    let mut guard = self.subscription.write().await;
+                    tracing::info!("Acknowledging ranges {:#?}.", self.offsets);
+                    guard.acknowledge(&self.partition, &self.offsets).unwrap();
+                }
+
+                // debug only, remove after
+                // {
+                //     tracing::info!("LOOK HERE ");
+                //     use crate::storage::index::windowed_index::WindowedIndex;
+                //     let mut bytes = [0u8; WindowedIndex::size_of()];
+                //     index_file.read_at(0, &mut bytes).unwrap();
+                //     for index in bytes
+                //         .chunks(WindowedIndex::size_of())
+                //         .map(WindowedIndex::of)
+                //     {
+                //         tracing::debug!("{:#?}", index);
+                //     }
+                // }
+
+                tracing::info!("Successfully applied ranges to windowed function.");
+            }
+            WindowValue::Timed((_count, _time_unit)) => {
+                tracing::error!("TIMED STREAM IS NOT AVAILABLE");
+
+                // ON timestamp type
+                //
+                //  -> We'd need to check the resultant offset's timestamp.
+                //  -> We would check if there are any "open" windows where this timestamp >
+                //  -> We add in these values and close the open ones that can be closed
+
+                todo!()
+            }
+        }
+
+        Ok(())
+    }
+}
 
 pub async fn create_windowed_stream_from_definition(
     definition: WindowedStreamDefinition,
@@ -22,7 +124,7 @@ pub async fn create_windowed_stream_from_definition(
     tracing::trace!("Calling create_windowed_stream_from_definition.");
     tracing::trace!("{:#?}", definition.base_key);
     let base_stream = definition.base_key.clone();
-    let stream = definition.resultant_key;
+    let stream = definition.resultant_key.clone();
 
     let (client_id, condvar, subscription) = {
         tracing::trace!("Attempting to input client_id.");
@@ -63,73 +165,18 @@ pub async fn create_windowed_stream_from_definition(
                 tracing::info!("Retrieved some offsets: {:#?}", offsets);
 
                 for (partition, offsets) in offsets.iter() {
-                    let resultant_stream = String::from_utf8(stream.as_bytes().to_vec()).unwrap();
+                    let mut operation = WindowOperation {
+                        broker: broker_ref.clone(),
+                        stream: stream.clone(),
+                        definition: definition.clone(),
+                        partition: partition.clone(),
+                        offsets: offsets.clone(),
+                        subscription: subscription.clone(),
+                    };
 
-                    tracing::info!("Retrieving index file for stream {resultant_stream}");
-
-                    // TODO: maybe paralellize these?
-                    let mut resultant_index_file =
-                        get_index_file_handle(&resultant_stream, partition, broker_ref.clone())
-                            .await;
-
-                    tracing::info!("Retrieved index file..");
-
-                    match definition.window_type {
-                        WindowValue::Count(count) => {
-                            let mut new_ranges = assign_sliding_windows_range(
-                                offsets.clone(),
-                                count,
-                                definition.slide.normalize(),
-                                0,
-                            );
-
-                            let mut guard = resultant_index_file.lock().await;
-                            let index_file = guard.as_index();
-
-                            let mut windowed_index_file = WindowedIndexFile::of(index_file);
-
-                            tracing::info!(
-                                "Applying ranges {:#?} to windowed index file.",
-                                new_ranges
-                            );
-
-                            windowed_index_file.put_ranges(&mut new_ranges).unwrap();
-
-                            // acknowledge me!
-                            {
-                                let mut guard = subscription.write().await;
-                                tracing::info!("Acknowledging ranges {:#?}.", offsets);
-                                guard.acknowledge(partition, offsets).unwrap();
-                            }
-
-                            // debug only, remove after
-                            // {
-                            //     tracing::info!("LOOK HERE ");
-                            //     use crate::storage::index::windowed_index::WindowedIndex;
-                            //     let mut bytes = [0u8; WindowedIndex::size_of()];
-                            //     index_file.read_at(0, &mut bytes).unwrap();
-                            //     for index in bytes
-                            //         .chunks(WindowedIndex::size_of())
-                            //         .map(WindowedIndex::of)
-                            //     {
-                            //         tracing::debug!("{:#?}", index);
-                            //     }
-                            // }
-
-                            tracing::info!("Successfully applied ranges to windowed function.");
-                        }
-                        WindowValue::Timed((_count, _time_unit)) => {
-                            tracing::error!("TIMED STREAM IS NOT AVAILABLE");
-
-                            // ON timestamp type
-                            //
-                            //  -> We'd need to check the resultant offset's timestamp.
-                            //  -> We would check if there are any "open" windows where this timestamp >
-                            //  -> We add in these values and close the open ones that can be closed
-
-                            todo!()
-                        }
-                    }
+                    operation.init().await.unwrap();
+                    operation.prepare().await.unwrap();
+                    operation.commit().await.unwrap();
                 }
             }
         })
