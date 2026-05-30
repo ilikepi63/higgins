0d530bd4b4bfa46f8015a523a26ea3e472892a5e
Cameron Harris <cameronh63@gmail.com>
2026-05-30 21:25:11 +0200

    chore(315): Completed Operation abstraction

--- higgins-core/src/derive/operation.rs
+++ higgins-core/src/derive/operation.rs
@@ -5,16 +5,47 @@
 use super::{
     joining::JoinOperation, map::MapOperation, reduce::ReduceOperation, windowed::WindowOperation,
 };
+use crate::error::HigginsError;
 
+#[allow(unused)]
 pub enum Step {
     Init,
     Prepare,
     Commit,
 }
 
+#[allow(unused)]
 enum Operation {
     Map(MapOperation),
     Reduce(ReduceOperation),
     Window(WindowOperation),
     Join(JoinOperation),
+}
+
+#[allow(unused)]
+impl Operation {
+    pub async fn init(&mut self) -> Result<(), HigginsError> {
+        match self {
+            Self::Map(o) => o.init().await,
+            Self::Join(o) => o.init().await,
+            Self::Window(o) => o.init().await,
+            Self::Reduce(o) => o.init().await,
+        }
+    }
+    pub async fn prepare(&mut self) -> Result<(), HigginsError> {
+        match self {
+            Self::Map(o) => o.prepare().await,
+            Self::Join(o) => o.prepare().await,
+            Self::Window(o) => o.prepare().await,
+            Self::Reduce(o) => o.prepare().await,
+        }
+    }
+    pub async fn commit(&mut self) -> Result<(), HigginsError> {
+        match self {
+            Self::Map(o) => o.commit().await,
+            Self::Join(o) => o.commit().await,
+            Self::Window(o) => o.commit().await,
+            Self::Reduce(o) => o.commit().await,
+        }
+    }
 }
